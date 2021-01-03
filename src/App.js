2a997f4c3c1657bc851c31f07b9eb2f5731fb3a5
Jordan <jordanhernando1085@gmail.com>
2021-01-03 00:05:45 -0300

    Componentes y routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from 'react';
+// Dependencias
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// Componentes
+import Login from './components/auth/Login';
+import NuevaCuenta from './components/auth/NuevaCuenta';
+import Proyectos from './components/proyectos/Proyectos';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>MERNTask</h1>
-    </div>
+    <Router>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+          <Route exact path="/proyectos" component={Proyectos} />
+        </Switch>
+      </Router>
+    </Router>
   );
 }
 
